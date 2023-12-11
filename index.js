1fdee221a44692a40e7b2cfb8f712046d162c180
HaiderRafi <haiderrafi.7@gmail.com>
2023-12-11 13:55:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// console.log("chai aur codeeee");
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send("<h1>Hello Twitter</h1>")
+})
+
+app.get('/login',(req,res)=>{
+    res.send("please login with yout credentials")
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
